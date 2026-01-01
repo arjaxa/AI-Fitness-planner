dfc7faf7de18b3089f67d2a54da4981a51b66df3
arjaxa <ariana.kh21@gmail.com>
2026-01-01 17:19:43 +0330

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ goal_map = {
     "Strength": "strength"
 }
 
-#goal_ui = st.selectbox("What's your main goal?", ["Hyperthrophy (Muscle Gain)", "Strength", "Fat Loss"])
+
 goal = goal_map[goal]
 
 # experience level
@@ -88,12 +88,6 @@ days_per_week = st.selectbox("How many days a week do you want to work out?", ["
 templates = custom_splits[days_per_week][goal][experience_key]
 selected_template = random.choice(templates)
 
-
-# Show split variations only for that day count
-#available_variations = list(custom_splits[days_per_week].keys())
-#selected_split = st.selectbox("Choose your split variation:", available_variations)
-
-#selected_split_data = custom_splits[days_per_week][selected_split]
 
 
 currunt_selection = (days_per_week, selected_template)
@@ -171,7 +165,7 @@ if st.button("Generate plan"):
     selected_template = random.choice(templates)
     st.session_state.generated_plan = generate_plan(selected_template, goal, experience_key)
     st.session_state.show_download = False
-    #st.session_state.pdf_file = None
+    
 
 
 # Display plan
@@ -200,68 +194,4 @@ if st.session_state.generated_plan:
             st.session_state.pdf_ready = False
         
                
-        
-   # for day, exercises in selected_split_data.items():
-   #     st.subheader(day)
-   #     st.divider()
-   #     for i, ex in enumerate(exercises, start=1):
-   #         if "|" in ex:
-   #             name, details = ex.split("|", 1)
-   #             st.markdown(f"**{i}. {name.strip()}**\n_{details.strip()}_")
-   #         else:
-   #             st.markdown(f"**{i}. {ex}**")
-        
-        #for i, ex in enumerate(exercises, start=1):
-        #    st.write(f"{i}. {ex}")
-
-
-#if st.button("Generate Plan"):
- #   selected_plan = custom_splits[days_per_week][selected_split]
-  #  for day, exercises_list in selected_plan.items():
-   #     st.subheader(day)
-    #    for i, exercise_group in enumerate(exercises_list, start=1):
-            # Pick one random workout from each exercise group
-     #       if isinstance(exercise_group, list):
-      #          chosen_ex = random.choice(exercise_group)
-       #     else:
-        #        chosen_ex = exercise_group
-         #   st.write(f"{i}. {chosen_ex}")
-
-
-#    selected_plan = custom_splits[days_per_week][selected_split]
-#   for day, exercises_list in selected_plan.items():
-#        st.subheader(day)
-#        for i, ex in enumerate(exercises_list, start=1):
-#            st.write(f"{i}. {ex}")
-
-#split_names = list(custom_splits.keys())
-#selected_split = st.selectbox("Select a split variation:", split_names)
-
-#if st.button("Generate Custom Plan"):
-#    selected_plan = custom_splits[selected_split]
-#    for day, exercises_list in selected_plan.items():
-#        st.subheader(day)
-#        for i, ex in enumerate(exercises_list, start=1):
-#            st.write(f"{i}. {ex}")
-
-
-#days = st.selectbox("How many days per week do you want to work out?", [3, 4, 5, 6])
-#split_choice = st.selectbox("Choose your workout split:", list(splits.keys()))
-
-#if st.button("Generate Plan"):
-#    chosen_split = splits[split_choice]
-#    plan = {}
-
-    # Cycle through the split for the number of workout days
-#    for i in range(days):
-#        muscles = chosen_split[i % len(chosen_split)]
-#        day_plan = []
-#        for m in muscles:
-#            day_plan.append(f"{m.capitalize()}: {random.choice(exercises.get(m, []))}")
-#        plan[f"Day {i+1}"] = day_plan
-
-#    st.success(f"✅ Generated {days}-Day {split_choice} Plan")
-#    for day, workouts in plan.items():
-#        st.subheader(day)
-#        for w in workouts:
-#            st.write(f"- {w}")
+    
